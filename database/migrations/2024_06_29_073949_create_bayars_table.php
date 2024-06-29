d31a4036c99ba3532dedc4004de46f5e06398b6e
Fikri <ahmad.fikrinew2801@gmail.com>
2024-06-29 14:47:23 +0700

    initialisasi project REST API Backend Mobile Aps Mobile Kopi

---
+++ database/migrations/2024_06_29_073949_create_bayars_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBayarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bayars', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_transaksi', 50)->unique();
+            $table->integer('total_belanja');
+            $table->integer('total_bayar');
+            $table->enum('status_bayar', ['tunai', 'non_tunai']);
+            $table->integer('id_pegawai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bayars');
+    }
+}
